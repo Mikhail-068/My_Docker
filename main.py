b937181b3469803dffe0d452b372b795a3c28f6d
Mihail_AI <m89531276499@gmail.com>
2022-12-17 14:16:18 +0300

    калькулятор

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-text = '''
-========================= Доброе утро, Михал Сергеич ===========================
+import argparse
 
-         -- Как вы сегодня рано встали, за самогон переживаете ? --
+text = '''
+===== Калькулятор =====
 '''.strip()
 
 
-print(text)
+def calc(x, y):
+    print(text)
+    res = x + y
+    print(f'{x} + {y} = {res}', end='\n\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Калькулятор') # Создаем парсер для аргументов, переданных через командную строку
+    parser.add_argument('x', type=int, help='Первое число')   # Задаем один обязательный аргумент - имя
+    parser.add_argument('y', type=int, help='Второе число')
+    args = parser.parse_args()
+    x = args.x
+    y = args.y
+    calc(x, y)
